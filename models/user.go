147d4e32039b1c3844fc834d23a8a49c5fd91195
runsaruol@gmail.com <runsaruol@gmail.com>
2023-07-03 00:50:33 +0700

    feat: buoi3

--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type UserModel struct {
-	ID       uint   `gorm:"primarykey"`
-	UserName string `gorm:"column:username" bson:"username"`
-	Email    string `gorm:"column:email" bson:"email"`
-	Phone    string `gorm:"column:phone" bson:"phone"`
-}
